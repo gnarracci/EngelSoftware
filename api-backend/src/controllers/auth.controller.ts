b6e8b80399b0acaf8c25f37750601a43215f7cd1
Gianni Narracci <gnarracci@gmail.com>
2023-12-17 09:23:29 -0400

    Auth.Controller Updates

--- api-backend/src/controllers/auth.controller.ts
+++ api-backend/src/controllers/auth.controller.ts
@@ -1,64 +1,66 @@
 import { Request, Response } from "express";
-import User, { IUser } from "../models/User";
-import Role, { IRole } from "../models/Role";
+import User from "../models/User";
+import Role from "../models/Role";
 import Company from "../models/Company";
 import jwt from "jsonwebtoken";
 
-
 export const signup = async (req: Request, res: Response) => {
+  const { username, email, password, role, company } = req.body;
 
-  const {username, email, password, role, company } = req.body;
-
-  // If User prevously exits
-
-
-  // User Save
-  const user: any = new User({
-    username,
-    email,
-    password
-  });
-
-  // Role Setuo
-  if(role) {
-    const foundRoles = await Role.find({name: {$in: role}})
-    user.role = foundRoles.map(role => role._id)
-    
+  // If User previously exits
+  const user = await User.findOne({ username: req.body.username });
+  if (user) return res.status(400).json({ message: "Username has benn used!" });
+  const mail = await User.findOne({ email: req.body.email });
+  if (mail && !user) {
+    return res.status(400).json({ message: "Email has been used." });
   } else {
-    const role = await Role.findOne({name: "user"})
-    user.role = [role?._id]
-    
-  }
-  
-  // Company Setup
-  if(company) {
-    const foundCompany = await Company.find({name: {$in: company}})
-    user.company = foundCompany.map(company => company._id)
+    // User Save
+    const user: any = new User({
+      username,
+      email,
+      password,
+    });
 
-  }else {
-    const company = await Company.findOne({name: "Test Company"})
-    user.company = [company?._id]
-  }
+    // Role Setuo
+    if (role) {
+      const foundRoles = await Role.find({ name: { $in: role } });
+      user.role = foundRoles.map((role) => role._id);
+    } else {
+      const role = await Role.findOne({ name: "user" });
+      user.role = [role?._id];
+    }
+
+    // Company Setup
+    if (company) {
+      const foundCompany = await Company.find({ name: { $in: company } });
+      user.company = foundCompany.map((company) => company._id);
+    } else {
+      const company = await Company.findOne({ name: "Test Company" });
+      user.company = [company?._id];
+    }
 
     // Password Encryption
-  user.password = await user.encryptPassword(user.password);
+    user.password = await user.encryptPassword(user.password);
 
     // Save New User
-  const savedUser = await user.save();
+    const savedUser = await user.save();
 
     // Token
-  const token: string = jwt.sign(
-    { _id: savedUser._id },
-    process.env.TOKEN_SECRET || "token_secret",
-    {
-      expiresIn: 28800, // Token duration "8" hours
-    }
-  );
-  res.header("auth-token", token).json(savedUser);
+    const token: string = jwt.sign(
+      { _id: savedUser._id },
+      process.env.TOKEN_SECRET || "token_secret",
+      {
+        expiresIn: 28800, // Token duration "8" hours
+      }
+    );
+    res.header("auth-token", token).json(savedUser);
+  }
 };
 
 export const signin = async (req: Request, res: Response) => {
-  const user = await User.findOne({ username: req.body.username }).populate("role").populate("company");
+  const user = await User.findOne({ username: req.body.username })
+    .populate("role")
+    .populate("company");
   if (!user) return res.status(400).json("User is wrong or not exits");
 
   const correctPassword: boolean = await user.validatePassword(
@@ -77,7 +79,9 @@ export const signin = async (req: Request, res: Response) => {
 };
 
 export const profile = async (req: Request, res: Response) => {
-  const user = await User.findById(req.userId, { password: 0 }).populate("role").populate("company");
+  const user = await User.findById(req.userId, { password: 0 })
+    .populate("role")
+    .populate("company");
   if (!user) return res.status(404).json("No User found");
   res.json(user);
 };
