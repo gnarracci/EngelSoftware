5272e720200bcd142822246e740d956bff5053ab
Gianni Narracci <gnarracci@gmail.com>
2024-03-11 15:26:22 -0400

    Template Component Setup

--- client/src/app/pages/templates/templates.component.ts
+++ client/src/app/pages/templates/templates.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CompanyService } from 'src/app/services/company/company.service';
 import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
@@ -10,17 +11,29 @@ export class TemplatesComponent implements OnInit{
 
   userLogged: any = {};
 
-  constructor(private userService: UserService) {}
+  companyData: any = {};
+
+  constructor(private userService: UserService, private companyServie: CompanyService) {}
 
   ngOnInit(): void {
     this.dataUser();
+    this.getCompanies();
   }
 
   dataUser() {
     this.userService.getProfile().subscribe(
       res => {
         this.userLogged = res;
-        console.log('USER', this.userLogged);
+        console.log('Company', this.userLogged.company[0].name);
+      }
+    )
+  }
+
+  getCompanies() {
+    this.companyServie.getCompanies().subscribe(
+      res => {
+        this.companyData = res;
+        console.log('COMPANIES', this.companyData);
       }
     )
   }
