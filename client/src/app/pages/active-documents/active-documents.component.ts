750ace119e1f863f39e70dbc7e4f7ca174ccfc93
Gianni Narracci <gnarracci@gmail.com>
2024-06-22 22:30:14 -0400

    Data Template storing setup

--- client/src/app/pages/active-documents/active-documents.component.ts
+++ client/src/app/pages/active-documents/active-documents.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 import { LoginService } from 'src/app/services/auth/login.service';
 import { ObjectsService } from 'src/app/services/objects/objects.service';
 import { UserService } from 'src/app/services/user/user.service';
@@ -30,12 +31,29 @@ export class ActiveDocumentsComponent implements OnInit {
   longer: any;
 
   showForm: any;
-
+  
   constructor(
     private loginService: LoginService,
     private userService: UserService,
-    private objsService: ObjectsService
+    private objsService: ObjectsService,
+    private formBuilder: FormBuilder
   ) {}
+
+  tForm = this.formBuilder.group({
+    code: [''],
+    adm: [''],
+    company: [''],
+    plant_name: [''],
+    plant_type: [''],
+    plant_code: [''],
+    0: [''],
+    1: [''],
+    2: [''],
+    3: [''],
+    4: [''],
+    5: [''],
+    6: [''],
+  });
 
   ngOnInit(): void {
     this.loginService.currentUserLoginOn.subscribe({
@@ -55,6 +73,10 @@ export class ActiveDocumentsComponent implements OnInit {
       },
       (err) => console.error(err)
     );
+  }
+
+  saveTemplate() {
+    console.log('TEMPLATEDATA', this.tForm.value);
   }
 
   getObjs() {
